189aa936206e8010d03128d1589c2fae66ffb348
Huigen Chen Chen <huigenchen1@gmail.com>
2026-04-15 11:12:45 +0200

    Ejercicio sorteo

---
+++ PooAvanzado/src/Sorteos/dado.java
@@ -0,0 +1,9 @@
+package Sorteos;
+
+public class dado extends sorteo{
+
+    @Override
+    public void lanzar() {
+
+    }
+}
---
+++ PooAvanzado/src/Sorteos/sorteo.java
@@ -0,0 +1,6 @@
+package Sorteos;
+
+public abstract class sorteo {
+    protected int posibilidades;
+    public abstract void lanzar();
+}
